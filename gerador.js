d985fadb6ecd02230a5f78474cb9e5cc944c8927
= <=>
2018-06-16 12:11:55 -0300

    icms

--- gerador.js
+++ gerador.js
@@ -3,8 +3,8 @@ var fs = require('fs'),
     path = require('path'),
     firebird = require('node-firebird'),
     buscaCep = require('busca-cep')
-    conexao = require('./db'),
-    nfe = require('./app'),    
+conexao = require('./db'),
+    nfe = require('./app'),
     NFe = nfe.NFe,
     Gerador = nfe.Gerador,
     Danfe = nfe.Danfe,
@@ -25,9 +25,8 @@ var transportador = new Transportador();
 var volumes = new Volumes();
 var danfe = new NFe();
 
-
 // abre os dados da empresa emitente da nota informada pelo sistema (#param.codigo)
-function dadosEmitente(empresa) {
+function dadosEmitente(empresa, venda) {
     return new Promise((resolve, reject) => {
         firebird.attach(conexao, function (err, db) {
             if (err) throw err;
@@ -50,19 +49,21 @@ function dadosEmitente(empresa) {
                             .comCidade(result[0].CIDADE)
                             .comCodMunicipio(result[0].CODIBGE)
                             .comUf(result[0].ESTADO));
-                    resolve();
+                    console.log(emitente);
+                    resolve(venda, emitente);
                 })
             })
         })
-    })
+    });
 }
-function dadosNota(venda) {
+function dadosNota(venda, emitente) {
     return new Promise((resolve, reject) => {
         firebird.attach(conexao, function (err, db) {
             if (err) throw err;
             db.query('select v.lcto,v.codcli,v.total,tr.peso,tr.volumes,tr.frete,tr.outra_desp,tr.desconto,tr.total_nota,tr.tipofrete,c.cgc,c.razao,c.insc,c.endereco,c.numero,c.bairro,c.complemento,c.cidade,c.cep,c.fone,c.email,ci.codibge,c.codcidade,ci.estado,ci.cod_estado,transp.codigo as codtransp, transp.transportador from venda v join transito tr on v.lcto = tr.documento join cliente c on c.codigo=v.codcli join cidade ci on c.codcidade = ci.cod_cidade left join transp on tr.codtransp = transp.codigo where lcto = ?', venda, function (err, result) {
                 if (err) throw err;
                 db.detach(function () {
+                    endDest = result[0].ESTADO;
                     destinatario.comNome(result[0].RAZAO)
                         .comRegistroNacional(result[0].CGC)
                         .comInscricaoEstadual(result[0].INSC)
@@ -85,40 +86,98 @@ function dadosNota(venda) {
                     volumes.comMarca('');
                     volumes.comNumeracao('');
                     volumes.comPesoBruto(result[0].PESO);
-                    resolve();
+                    console.log(destinatario);
+                    resolve(venda);
                 })
             })
         })
     })
 }
 
-dadosEmitente(1).then(dadosNota(1358273)).then(function () {
-    // console.log(destinatario)
-    danfe.comChaveDeAcesso('52131000132781000178551000000153401000153408');
-    danfe.comEmitente(emitente);
-    danfe.comDestinatario(destinatario);
-    danfe.comTransportador(transportador);
-    // danfe.comProtocolo(protocolo);
-    danfe.comImpostos(impostos);
-    danfe.comVolumes(volumes);
-    danfe.comTipo('saida');
-    danfe.comNaturezaDaOperacao('VENDA');
-    danfe.comNumero(1420);
-    danfe.comSerie(100);
-    danfe.comDataDaEmissao(new Date(2014, 10, 19));
-    danfe.comDataDaEntradaOuSaida(new Date(2014, 10, 19, 12, 43, 59));
-    danfe.comModalidadeDoFrete('porContaDoDestinatarioRemetente');
-    danfe.comInscricaoEstadualDoSubstitutoTributario('102959579');
-    danfe.comInformacoesComplementares('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum nihil aut nostrum');
-    danfe.comValorTotalDaNota(250.13);
-    danfe.comValorTotalDosProdutos(120.10);
-    danfe.comValorTotalDosServicos(130.03);
-    danfe.comValorDoFrete(23.34);
-    danfe.comValorDoSeguro(78.65);
-    danfe.comDesconto(1.07);
-    danfe.comOutrasDespesas(13.32);
+function criaNf(venda) {
+    return new Promise((resolve, reject) => {
+        danfe.comChaveDeAcesso('52131000132781000178551000000153401000153408');
+        danfe.comEmitente(emitente);
+        danfe.comDestinatario(destinatario);
+        danfe.comTransportador(transportador);
+        // danfe.comProtocolo(protocolo);
+        danfe.comImpostos(impostos);
+        danfe.comVolumes(volumes);
+        danfe.comTipo('saida');
+        danfe.comNaturezaDaOperacao('VENDA');
+        danfe.comNumero(1420);
+        danfe.comSerie(100);
+        danfe.comDataDaEmissao(new Date(2014, 10, 19));
+        danfe.comDataDaEntradaOuSaida(new Date(2014, 10, 19, 12, 43, 59));
+        danfe.comModalidadeDoFrete('porContaDoDestinatarioRemetente');
+        danfe.comInscricaoEstadualDoSubstitutoTributario('102959579');
+        danfe.comInformacoesComplementares('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum nihil aut nostrum');
+        danfe.comValorTotalDaNota(250.13);
+        danfe.comValorTotalDosProdutos(120.10);
+        danfe.comValorTotalDosServicos(130.03);
+        danfe.comValorDoFrete(23.34);
+        danfe.comValorDoSeguro(78.65);
+        danfe.comDesconto(1.07);
+        danfe.comOutrasDespesas(13.32);
+        resolve(venda);
+    })
 
-})
+}
+
+function itensNota(venda) {
+    return new Promise((resolve, reject) => {
+        console.log(danfe.getEmitente().getCrt())
+        console.log(danfe.getEmitente().getEndereco().getUf())
+        console.log(danfe.getDestinatario().getEndereco().getUf())
+
+        firebird.attach(conexao, function (err, db) {
+            if (err) throw err;
+            db.query('select * from listaprodvendas(?)', venda, function (err, result) {
+                if (err) throw err;
+                db.detach(function () {
+                    for (let item of result) {
+                        console.log(item.BASECALC)
+                        console.log(item.ALIQ)
+                        console.log(item.ORIG)
+                        var prodst = (item.SITTRIB === "060") ? true : false;
+                        var icms = new Icms().CalculaIcms(
+                            prodst,
+                            danfe.getEmitente().getCrt(),
+                            danfe.getEmitente().getEndereco().getUf(),
+                            danfe.getDestinatario().getEndereco().getUf(),
+                            1,
+                            1,
+                            item.BASECALC,
+                            item.ALIQ,
+                            item.ORIG
+                        )
+                        console.log(icms)
+                        danfe.adicionarItem(new Item()
+                            .comCodigo(item.CODIGO)
+                            .comDescricao(item.DESCRICAO)
+                            .comNcmSh(item.NCM)
+                            .comIcms(icms)
+                            // .comOCst('020')
+                            // .comCfop('6101')
+                            .comUnidade(item.UNIDADE)
+                            .comQuantidade(item.QTDPEDIDO)
+                            .comValorUnitario(item.VALOR)
+                            .comValorTotal(item.VALOR * item.QTDPEDIDO));
+                    }
+                    resolve(danfe);
+                })
+            })
+        })
+    })
+}
+
+
+
+
+
+//cadeia de execução
+dadosEmitente(1, 1358273).then(dadosNota).then(criaNf).then(itensNota).then(function (res) { console.log(danfe) });
+
 
 
 var protocolo = new Protocolo();
@@ -138,21 +197,8 @@ var impostos = new Impostos();
 // impostos.comValorTotalDoIssqn(30);
 
 
-for (var i = 0; i < 5; i++) {
-    var icms = new Icms().CalculaIcms(0, 2, 'SP', 'SP', 1, 1, '100', '18', '1')
-    danfe.adicionarItem(new Item()
-        .comCodigo('' + i)
-        .comDescricao('Produto')
-        .comNcmSh('15156000')
-        .comIcms(icms)
-        // .comOCst('020')
-        // .comCfop('6101')
-        .comUnidade('LT')
-        .comQuantidade(3.1415)
-        .comValorUnitario(2.31)
-        .comValorTotal(7.13));
-}
-        console.log(danfe._itens)
+
+
 
         // console.log(new Icms().CalculaIcms(0, 2, 'SP', 'SP', 1, 1, '100', '18', '1'))
 
